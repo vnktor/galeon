16a1fe987248071f59d456d65d9dec899ed61190
Victor <vnktor@mail.ru>
2019-01-15 13:35:54 +0600

    Проект. Ball

---
+++ Assets/scripts/RemoteControlBall.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RemoteControlBall : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private int count;
+    public Text textCont;
+    public Text winText;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetTextCount(count);
+        winText.text = "";
+    }
+
+    void FixedUpdate()  {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        float speed = 10.0f;
+
+        Vector3 movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+
+        rb.AddForce(movement*speed);
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+       if(other.gameObject.CompareTag("Cube"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetTextCount(count);
+        }
+    }
+
+    void SetTextCount(int count)
+    {
+        textCont.text = "Count :" + count.ToString();
+        if (count > 11)
+            winText.text = "Вы выйграли!";
+    }
+}
