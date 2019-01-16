f5f67303d48d51cbd21064fc424f960be9eb5bb5
Andrey Zero <89514643532@mail.ru>
2019-01-16 23:09:10 +0500

    roll a boll

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Controller : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    private int count;
+    // Text fields
+    public Text countText, winText;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        setCount ();
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter (Collider gold) { 
+        if (gold.tag == "Point") {
+            Destroy (gold.gameObject);
+            count++;
+            setCount();
+        }
+    }
+
+    void setCount () {
+        countText.text = "Gold: " + count.ToString ();
+        if (count >= 11)
+            winText.text = "You Win!";
+    }
+
+}
