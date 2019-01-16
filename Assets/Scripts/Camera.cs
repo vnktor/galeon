fc50d07f22901e1133a9419ef0483fc4412ea9ca
Andrey Zero <89514643532@mail.ru>
2019-01-16 18:36:08 +0500

    Roll a Boll

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public GameObject Player;
+    private Vector3 offset;
+
+    void Start() { 
+        offset = transform.position - Player.transform.position;
+       }
+
+    void LateUpdate() { 
+        transform.position = Player.transform.position + offset;
+    } 
+}
