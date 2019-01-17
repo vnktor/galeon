3d38d1ed7fc84a3c2b6ad34a32e6dc03a6fdcad2
sshevnin <sshevnin@yandex.ru>
2019-01-18 02:16:01 +0500

    Исправленно

--- RollABall/Assets/Scripts/CameraController.cs
+++ RollABall/Assets/Scripts/CameraController.cs
@@ -2,18 +2,18 @@
 
 public class CameraController : MonoBehaviour {
 
-    public GameObject player;
+	public GameObject player;
 
-    private Vector3 offset;
+	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - player.transform.position;
+		offset = transform.position - player.transform.position;
 	}
 
-    // Update is called once per frame
-    private void LateUpdate()
-    {
-        transform.position = player.transform.position + offset;
+	// Update is called once per frame
+	private void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
 	}
 }
--- RollABall/Assets/Scripts/Rotator.cs
+++ RollABall/Assets/Scripts/Rotator.cs
@@ -4,6 +4,6 @@ public class Rotator : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
 	}
 }
