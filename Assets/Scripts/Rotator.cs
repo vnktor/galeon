53e2d467b7e1b9fad365859e47d23fbed66bedf8
ssnshkl <11noabrya@mail.ru>
2019-01-18 03:25:51 +0500

    assets direction fix

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Roll_a_Ball {
+    public class Rotator : MonoBehaviour {
+
+        void Update() {
+            this.transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        }
+    }
+}
