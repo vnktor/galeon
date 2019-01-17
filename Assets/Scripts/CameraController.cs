7f3d8782bc4dfcbca2ce2f706ec510c082cbcad3
sshevnin <sshevnin@yandex.ru>
2019-01-18 02:31:46 +0500

    скопировал в папку Assets

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	private void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
