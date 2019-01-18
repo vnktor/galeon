d99a2ca9c5182a425cfec096a4fa497c17be9b5f
sshevnin <sshevnin@yandex.ru>
2019-01-18 15:33:01 +0500

    Исправлены файлы скриптов

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,14 +5,13 @@ public class CameraController : MonoBehaviour {
 	public GameObject player;
 	private Vector3 offset;
 
-    // Use this for initialization
-    private void Start () {
-        this.offset = this.transform.position - this.player.transform.position;
+	// Use this for initialization
+	private void Start () {
+		this.offset = this.transform.position - this.player.transform.position;
 	}
 
 	// Update is called once per frame
-	private void LateUpdate()
-	{
-        this.transform.position = this.player.transform.position + this.offset;
+	private void LateUpdate() {
+		this.transform.position = this.player.transform.position + this.offset;
 	}
 }
