9198ad217612c1cbfa997122652df686fa399889
Cpa3uk <zhelezniak.pp@gmail.com>
2019-01-20 16:47:40 +0500

    -добавл цель, к которой должен перемещаться мяч. -в CameraController сделал перемещение цели по клику мыши. -изменил перемещение мяча, стремится к цели, если ещё не достиг её. -при задании новой цели перемещения меняется цвет мяча.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+	public PlayerController player;
+	public Transform target;
+	public LayerMask mask;
 
-	public GameObject player;
 	private Vector3 offset;
 
-	private void Start (){
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			MouseClick();
+		}
+	}
+
+	private void MouseClick()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit[] hits = Physics.RaycastAll(ray, 50.0f, this.mask);
+		foreach (var hit in hits)
+		{
+			if (hit.transform.CompareTag("Terrain"))
+			{
+				this.target.position = hit.point;
+				this.player.NewTarget();
+				break;
+			}
+		}
+	}
+
+	private void Start ()
+	{
 		this.offset = this.transform.position - this.player.transform.position;
 	}
-		
-	private void LateUpdate (){
+
+	private void LateUpdate ()
+	{
 		this.transform.position = this.player.transform.position + this.offset;
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,42 +1,74 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
 	public float speed;
 	public Text countText;
 	public Text winText;
+	public Transform target;
 
 	private Rigidbody rb;
 	private int count;
+	private bool moveComplete;
+	private Renderer render;
 
-	private void Start(){
-		this.rb = GetComponent<Rigidbody>();
+	public void NewTarget()
+	{
+		moveComplete = false;
+		if (render.material.color == Color.green)
+		{
+			render.material.color = Color.red;
+		}
+		else
+		{
+			render.material.color = Color.green;
+		}
+	}
+
+	private void Start()
+	{
+		this.rb = this.GetComponent<Rigidbody>();
 		this.count = 0;
 		SetCountText();
 		this.winText.text = "";
+		this.render = this.GetComponent<Renderer>();
+		this.render.material.color = Color.red;
+		this.moveComplete = true;
 	}
 
-	private void FixedUpdate(){
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
-
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
-		this.rb.AddForce(movement * this.speed);
+	private void FixedUpdate()
+	{
+		if (!this.moveComplete)
+		{
+			if (Vector3.Distance(this.transform.position, this.target.position) < 1)
+			{
+				moveComplete = true;
+			}
+			else
+			{
+				Vector3 movement = new Vector3(target.position.x - this.transform.position.x, this.transform.position.z, target.position.z - this.transform.position.z);
+				movement.Normalize();
+				this.rb.AddForce(movement * this.speed);
+			}
+		}
 	}
 
-	private void OnTriggerEnter(Collider other){
-		if (other.gameObject.CompareTag("PickUp")){
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
 			other.gameObject.SetActive(false);
 			this.count++;
 			SetCountText();
 		}
 	}
 
-	private void SetCountText(){
+	private void SetCountText()
+	{
 		this.countText.text = "Count: " + this.count.ToString();
-		if (this.count >= 12){
+		if (this.count >= 12)
+		{
 			this.winText.text = "You Win!!!";
 		}
 	}
