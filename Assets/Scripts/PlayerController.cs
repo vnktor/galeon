d3fbaa23ddb76a2a8d92e352dd0a1bda205c1b9f
sshevnin <sshevnin@yandex.ru>
2019-01-20 23:30:14 +0500

    Задание №3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,18 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public Text countText;
 	public Text winText;
+	public Transform trPlayer;
 	private Rigidbody rb;
 	private int count;
+	private float UpdTimer;
 
 	private void Start() {
 		this.rb = GetComponent<Rigidbody>();
 		this.count = 0;
 		SetCountText();
 		this.winText.text = "";
+		this.UpdTimer = -1;
+		this.trPlayer.position=new Vector3(PlayerPrefs.GetFloat("PlayerX"),0.5f,PlayerPrefs.GetFloat("PlayerZ"));
 	}
 
 	private void FixedUpdate() {
@@ -21,7 +25,29 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis("Vertical");
 
 		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		this.rb.AddForce(movement* this.speed);
+		this.rb.AddForce(movement * this.speed);
+
+		if (Input.GetMouseButton(0)) {
+			// Нажатие левой кнопки
+			PlayerPrefs.SetFloat("PlayerX", this.trPlayer.position.x);
+			PlayerPrefs.SetFloat("PlayerZ", this.trPlayer.position.z);
+			this.rb.AddForce(transform.up * 25);
+		}
+
+		if (Input.GetMouseButton(1)) {
+			// Нажатие правой кнопки
+			this.trPlayer.localScale = new Vector3(0.6f, 0.6f, 0.6f);
+			this.UpdTimer = 0.15f;
+			this.trPlayer.position = new Vector3(PlayerPrefs.GetFloat("PlayerX"), 0.5f, PlayerPrefs.GetFloat("PlayerZ"));
+        }
+
+		if (this.UpdTimer != -1f) {
+			UpdTimer -= Time.deltaTime;
+			if (this.UpdTimer <= 0) {
+				this.trPlayer.localScale = new Vector3(1, 1, 1);
+				this.UpdTimer = -1f;
+			}
+		}
 	}
 
 	private void OnTriggerEnter(Collider other) {
