68e8fc83f830d6b6ecae4eb1296e73f7eb4d64f3
ssnshkl <11noabrya@mail.ru>
2019-01-24 16:40:29 +0500

    Models and new scripts add

---
+++ Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roll_a_Ball {
+	public class DestroyObject : MonoBehaviour {
+
+		void Update() {
+			if (Input.GetMouseButtonDown(0)) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
