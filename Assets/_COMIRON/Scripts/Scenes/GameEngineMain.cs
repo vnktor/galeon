0c669c555b1dbf7e622f8bddfa6e501c1fe62064
sshevnin <sshevnin@yandex.ru>
2019-01-27 19:39:54 +0500

    Менеджер машин

--- Assets/_COMIRON/Scripts/Scenes/GameEngineMain.cs
+++ Assets/_COMIRON/Scripts/Scenes/GameEngineMain.cs
@@ -1,6 +1,7 @@
 ﻿using COMIRON.GameFramework.Core;
 using COMIRON.Managers.ManagerMap;
 using COMIRON.Managers.ManagerRoads;
+using COMIRON.Managers.ManagerTransport;
 using COMIRON.Settings;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -29,7 +30,6 @@ namespace COMIRON.Scenes {
 				}
 			}
 			
-			
 			//managerRoads.CreateControllerRoadCorner(groundStartPosition + new Vector3(30, 0.1f, 30));
 			//managerRoads.CreateControllerRoadStraight(groundStartPosition + new Vector3(30, 0.1f, 30 + 7.62f));
 			var roadCreateResult = this.CreateRoad(RoadDirection.Up, groundStartPosition + new Vector3(40, 0.1f, 30), 5);
@@ -48,7 +48,8 @@ namespace COMIRON.Scenes {
 		
 		private RoadCreateResult CreateRoad(RoadDirection roadDirection, Vector3 startPosition, int length, int offset = 0) {
 			var managerRoads = this.GetManager<ManagerRoads>();
-			
+			var managerTransport = this.GetManager<ManagerTransport>();
+
 			Quaternion roadRotation = Quaternion.identity;
 			Vector3 roadAddPositionDirection = Vector3.zero;
 			switch (roadDirection) {
@@ -77,9 +78,14 @@ namespace COMIRON.Scenes {
 				
 				controllerRoadStraight.transform.localRotation = roadRotation;
 			}
-			
+
 			var controllerRoadCorner = managerRoads.CreateControllerRoadCorner(
 				startPosition + new Vector3(0, 0.1f, 0) + roadAddPositionDirection * 7.62f * (length + offset)
+			);
+
+			//Выводим машину в центр каждого угла
+			var controllerTransport = managerTransport.CreateControllerCar04(
+				startPosition + new Vector3(0, 0.15f, 0) + roadAddPositionDirection * 7.62f * (length + offset)
 			);
 			
 			var cornerPosition = controllerRoadCorner.transform.position;
---
+++ Assets/_COMIRON/Scripts/Settings/SettingsTransport.cs
@@ -0,0 +1,31 @@
+﻿using COMIRON.GameFramework.Core;
+using COMIRON.Managers.ManagerTransport;
+using UnityEngine;
+
+namespace COMIRON.Settings {
+	public class SettingsTransport : SettingsBase
+	{
+		[SerializeField]
+		private ControllerCar02 controllerCar02Prefab;
+		[SerializeField]
+		private ControllerCar03 controllerCar03Prefab;
+		[SerializeField]
+		private ControllerCar04 controllerCar04Prefab;
+
+		protected override void AwakeInherit() {
+
+		}
+
+		public ControllerCar02 GetControllerCar02Prefab() {
+			return this.controllerCar02Prefab;
+		}
+
+		public ControllerCar03 GetControllerCar03Prefab() {
+			return this.controllerCar03Prefab;
+		}
+
+		public ControllerCar04 GetControllerCar04Prefab() {
+			return this.controllerCar04Prefab;
+		}
+	}
+}
