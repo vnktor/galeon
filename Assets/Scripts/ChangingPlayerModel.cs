73835eee991de2b1c3730912ec25fbe6f9c094eb
ssnshkl <11noabrya@mail.ru>
2019-01-28 09:23:12 +0500

    Tabulation fix

--- Assets/Scripts/ChangingPlayerModel.cs
+++ Assets/Scripts/ChangingPlayerModel.cs
@@ -5,27 +5,25 @@ using UnityEngine;
 namespace Roll_a_Ball {
 	public class ChangingPlayerModel : MonoBehaviour {
 
-		public GameObject newMainModel;
-		public GameObject cubeModel;
-		public GameObject cylinderModel;
-		public GameObject sphereModel;
-		public int counter;
+		private GameObject newMainModel;
+		private GameObject cubeModel;
+		private GameObject cylinderModel;
+		private GameObject sphereModel;
+		private int counter;
 
-        void Start() {
-	        this.sphereModel = Resources.Load("PlayerModels/Sphere") as GameObject;
-            this.cubeModel = Resources.Load("PlayerModels/Cube") as GameObject;
-            this.cylinderModel = Resources.Load("PlayerModels/Cylinder") as GameObject;
+		private void Start() {
+			this.sphereModel = Resources.Load("PlayerModels/Sphere") as GameObject;
+			this.cubeModel = Resources.Load("PlayerModels/Cube") as GameObject;
+			this.cylinderModel = Resources.Load("PlayerModels/Cylinder") as GameObject;
 			Debug.Log("------ " + newMainModel);
 		}
 
-		void Update() {
-			if (Input.GetMouseButtonDown(1))
-			{
+		private void Update() {
+			if (Input.GetMouseButtonDown(1)) {
 				counter++;
 			}
 
-			switch (counter)
-			{
+			switch (counter) {
                 case 1:
 				var a = Instantiate(cubeModel);
 				a.transform.SetParent(this.transform);
@@ -46,21 +44,21 @@ namespace Roll_a_Ball {
 				this.newMainModel = b;
 				break;
 
-                case 3:
-	            var c = Instantiate(sphereModel);
-	            c.transform.SetParent(this.transform);
-	            c.transform.localPosition = new Vector3();
+				case 3:
+				var c = Instantiate(sphereModel);
+				c.transform.SetParent(this.transform);
+				c.transform.localPosition = new Vector3();
 
-	            GameObject.Destroy(this.newMainModel);
+				GameObject.Destroy(this.newMainModel);
 
-	            this.newMainModel = c;
-	            break;
+				this.newMainModel = c;
+				break;
 
-                case 4:
+				case 4:
 				GameObject.Destroy(this.newMainModel);
 				counter = 1;
 				break;
-            }
+			}
 		}
 	}
 }
