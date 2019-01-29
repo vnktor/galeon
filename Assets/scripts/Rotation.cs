9bcaba9ee9c23976375bcb0cf6dcd694b1d8c62a
West74 <46949904+West74@users.noreply.github.com>
2019-01-29 11:39:19 +0500

    Update Rotation.cs

--- Assets/scripts/Rotation.cs
+++ Assets/scripts/Rotation.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour
-{
-    void Update()
-    {
+public class Rotation : MonoBehaviour {
+    private void Update() {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
