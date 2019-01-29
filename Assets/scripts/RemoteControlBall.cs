ac10a544f26673e91c5487bb5c2343de9b039815
West74 <46949904+West74@users.noreply.github.com>
2019-01-29 11:38:25 +0500

    Update RemoteControlBall.cs

--- Assets/scripts/RemoteControlBall.cs
+++ Assets/scripts/RemoteControlBall.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class RemoteControlBall : MonoBehaviour
-{
-
+public class RemoteControlBall : MonoBehaviour {
     private Rigidbody rb;
     private int count;
     public Text textCont;
     public Text winText;
 
-    // Start is called before the first frame update
-    void Start() {
+    private void Start() {
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetTextCount(count);
@@ -24,24 +21,19 @@ public class RemoteControlBall : MonoBehaviour
         float MoveHorizontal = Input.GetAxis("Horizontal");
         float MoveVertical = Input.GetAxis("Vertical");
         float speed = 10.0f;
-
         Vector3 movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
-
         rb.AddForce(movement*speed);
     }
 
-
     private void OnTriggerEnter(Collider other) {
-       if(other.gameObject.CompareTag("Cube"))
-        {
+        if(other.gameObject.CompareTag("Cube")) {
             other.gameObject.SetActive(false);
             count++;
             SetTextCount(count);
         }
     }
 
-    void SetTextCount(int count)
-    {
+    private void SetTextCount(int count) {
         textCont.text = "Count :" + count.ToString();
         if (count > 11)
             winText.text = "Вы выйграли!";
