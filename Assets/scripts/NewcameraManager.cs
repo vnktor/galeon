cdb90c6ef676866ae2a116e247464a6ded9de6fd
West74 <46949904+West74@users.noreply.github.com>
2019-01-29 11:40:20 +0500

    Update NewcameraManager.cs

--- Assets/scripts/NewcameraManager.cs
+++ Assets/scripts/NewcameraManager.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewcameraManager : MonoBehaviour
-{
+public class NewcameraManager : MonoBehaviour {
     public GameObject player;
-
     private Vector3 offset;
     
-    void Start() {
+    private void Start() {
         offset = transform.position - player.transform.position;
     }
 
-    // Update is called once per frame
     void LateUpdate() {
         transform.position = player.transform.position+offset;
     }
