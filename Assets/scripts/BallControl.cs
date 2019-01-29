ed2484a57fd71948c3e163af41d0e5ae948be2eb
West74 <46949904+West74@users.noreply.github.com>
2019-01-29 11:42:49 +0500

    Update BallControl.cs

--- Assets/scripts/BallControl.cs
+++ Assets/scripts/BallControl.cs
@@ -4,43 +4,35 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class BallControl : MonoBehaviour {
-
     private Rigidbody rb;
     private int count;
     public Text textCont;
     public Text winText;
 
-    void Start()
-    {
+    private void Start() {
         rb = GetComponent<Rigidbody>();
         count = 0;
         SetTextCount(count);
         winText.text = "";
     }
 
-    void FixedUpdate()
-    {
+    private void FixedUpdate() {
         float MoveHorizontal = Input.GetAxis("Horizontal");
         float MoveVertical = Input.GetAxis("Vertical");
         float speed = 10.0f;
-
         Vector3 movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
         rb.AddForce(movement * speed);
     }
 
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Cube"))
-        {
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Cube")) {
             other.gameObject.SetActive(false);
             count++;
             SetTextCount(count);
         }
     }
 
-    void SetTextCount(int count)
-    {
+    private void SetTextCount(int count) {
         textCont.text = "Count :" + count.ToString();
         if (count > 11)
             winText.text = "Вы выйграли!";
