184f5882c7518d293e6b87c92b486f2b198659a8
Andrey Zero <89514643532@mail.ru>
2019-02-01 00:43:56 +0500

    Добавленны файлы по задаче с анимацией.

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -3,43 +3,41 @@ using UnityEngine.UI;
 
 namespace RollaBoll {
 	public class Player_Controller : MonoBehaviour {
-		public float speed;
 		
 		private Rigidbody rb;
 		
 		private int count;
-// Text fields
+		
+		public Transform player;
+		
+		public Transform animObject;
+		
 		public Text countText, winText;
 		
-		private void Start() {
+		void Start() {
 			rb = GetComponent<Rigidbody>();
 			count = 0;
 			winText.text = "";
-			SetCount ();
+			setCount ();
 		}
 		
 		private void FixedUpdate() {
-			float moveHorizontal = Input.GetAxis("Horizontal");
-			float moveVertical = Input.GetAxis("Vertical");
-			
-			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-			
-			this.rb.AddForce(movement * speed);
+
+			animObject.transform.position = player.transform.position;
 		}
 		
 		private void OnTriggerEnter (Collider gold) { 
 			if (gold.tag == "Point") {
-				Destroy (gold.gameObject);
-				this.count++;
-				SetCount();
-			}
+			Destroy (gold.gameObject);
+			this.count++;
+			setCount();
 		}
+	}
 		
-		private void SetCount () {
+		private void setCount () {
 			this.countText.text = "Gold: " + count.ToString ();
-			if (this.count >= 11) {
-				this.winText.text = "You Win!";
-			}
+			if (this.count >= 11)
+			this.winText.text = "You Win!";
 		}
 	}
 }
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -3,7 +3,7 @@
 namespace RotatePoint {
 	public class Rotate : MonoBehaviour {
 		
-		private void Update () {
+		void Update () {
 			transform.Rotate (new Vector3(190, 0, 0) * Time.deltaTime);
 		}
 	}
