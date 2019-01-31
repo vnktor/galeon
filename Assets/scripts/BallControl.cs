cd305dd323e33030f66eefa46be15996c7772b34
west <a@a.com>
2019-01-31 20:57:16 +0600

    .

--- Assets/scripts/BallControl.cs
+++ Assets/scripts/BallControl.cs
@@ -6,82 +6,79 @@ using UnityEngine.UI;
 public class BallControl : MonoBehaviour {
 	public Text textCont;
 	public Text winText;
-	private Rigidbody rb;
-	private int count;
-	private Ray ray;
-	private RaycastHit[] hits;
-	private bool FlagClickMouse;				// флаг состояния объекта true- полный размер \false -исчез
-	private Vector3 StartScale;					// начальный маштаб объекта
-	private Vector3 CurrentScale;
-	private Vector3 CurrentPointRescale;
-	private bool isReScale;						// флаг выполнения плавной трансформации
-	private float SpeedReScale;
-	private int WayRescale;
+	public Camera aCurrentCamera;
+	private int aCount;
+	private Ray aRay;
+	private RaycastHit[] aHits;
+	private bool aFlagClickMouse;
+	private Vector3 aStartScale;
+	private Vector3 aCurrentScale;
+	private Vector3 aCurrentPointRescale;
+	private bool isReScale;
+	private float aSpeedReScale;
+	private int aWayRescale;
 
 	private void Start() {
-		this.rb = GetComponent<Rigidbody>();
-		this.count = 0;
-		this.SetTextCount(this.count);
+		this.aCount = 0;
+		this.SetTextCount(this.aCount);
 		this.winText.text = "";
-		this.FlagClickMouse = true;
-		this.StartScale= transform.localScale;
+		this.aFlagClickMouse = true;
+		this.aStartScale= transform.localScale;
 		this.isReScale = false;
-		this.SpeedReScale = 2f;
-		
+		this.aSpeedReScale = 2.0f;
 	}
 
 	private void Update() {
-		// Обрабатываем нажатие кнопки
 		if (Input.GetMouseButtonDown(0) && !this.isReScale) {
-			// создаем луч из позиции камеры в сторону клика мыши
-			this.ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
-			//получаем пересечение со всеми объектами
-			this.hits = UnityEngine.Physics.RaycastAll(this.ray);
-			for (int i=0;i<this.hits.Length;i++) {
-				if (this.hits[i].collider.gameObject.CompareTag("MainPlane")) {
-					if (this.FlagClickMouse) {	// скрытие объекта
-						this.FlagClickMouse = false;
+			this.aRay = this.aCurrentCamera.ScreenPointToRay(Input.mousePosition);
+			this.aHits = UnityEngine.Physics.RaycastAll(this.aRay);
+			for (int i = 0; i < this.aHits.Length; i++) {
+				if (this.aHits[i].collider.gameObject.CompareTag("MainPlane")) {
+					if (this.aFlagClickMouse) {
+						this.aFlagClickMouse = false;
 						this.SetMakeReScale(-1, new Vector3(this.transform.position.x, 0, this.transform.position.z));
 					}
-					else {						// перемещение и восстановление размера
-						this.FlagClickMouse = true;
-						this.SetMakeReScale(1, new Vector3(this.hits[i].point.x, this.StartScale.y / 2.0f, this.hits[i].point.z));
+					else {
+						this.aFlagClickMouse = true;
+						this.SetMakeReScale(1, new Vector3(this.aHits[i].point.x, this.aStartScale.y / 2.0f, this.aHits[i].point.z));
 					}
 				}
 			}
 		}
+	}
+
+	private void FixedUpdate() {
 		if(this.isReScale) {
 			this.MakeReScale();
 		}
 	}
 
-	// запуск плавного изменения Scale
 	private void SetMakeReScale(int aWayReScale,Vector3 PointReScale) {
 		this.isReScale = true;
-		this.WayRescale = aWayReScale;
-		this.CurrentScale=this.transform.localScale;
-		this.CurrentPointRescale = PointReScale;
+		this.aWayRescale = aWayReScale;
+		this.aCurrentScale=this.transform.localScale;
+		this.aCurrentPointRescale = PointReScale;
 	}
 
 	private void MakeReScale() {
-		this.CurrentScale += this.StartScale * this.SpeedReScale * Time.deltaTime * this.WayRescale;
-		if (this.CurrentScale.x <= 0) {
-			this.CurrentScale = new Vector3(0, 0, 0);
+		this.aCurrentScale += this.aStartScale * this.aSpeedReScale * Time.deltaTime * this.aWayRescale;
+		if (this.aCurrentScale.x <= 0) {
+			this.aCurrentScale = new Vector3(0, 0, 0);
 			this.isReScale = false;
 		}
-		if (this.CurrentScale.x >= this.StartScale.x) {
-			this.CurrentScale = this.StartScale;
+		if (this.aCurrentScale.x >= this.aStartScale.x) {
+			this.aCurrentScale = this.aStartScale;
 			this.isReScale = false;
 		}
-		this.transform.position = (new Vector3(this.CurrentPointRescale.x, this.CurrentScale.y / 2.0f, this.CurrentPointRescale.z));
-		this.transform.localScale = (Vector3)this.CurrentScale;
+		this.transform.position = (new Vector3(this.aCurrentPointRescale.x, this.aCurrentScale.y / 2.0f, this.aCurrentPointRescale.z));
+		this.transform.localScale = (Vector3)this.aCurrentScale;
 	}
 
 	private void OnTriggerEnter(Collider other) {
 		if(other.gameObject.CompareTag("Cube")) {
 			other.gameObject.SetActive(false);
-			this.count++;
-			this.SetTextCount(this.count);
+			this.aCount++;
+			this.SetTextCount(this.aCount);
 		}
 	}
 
