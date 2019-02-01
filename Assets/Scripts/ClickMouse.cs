8e7d3d8eb252c00d5009baeb980fa1f09cfa3913
Andrey Zero <89514643532@mail.ru>
2019-02-01 18:17:26 +0500

    Добавил недостающие скрипты

---
+++ Assets/Scripts/ClickMouse.cs
@@ -0,0 +1,26 @@
+﻿using RollaBall;
+using System.Collections;
+using UnityEngine;
+
+namespace CkicktoPlayer {
+	public class ClickMouse : MonoBehaviour {
+		public Animation animObject;
+		
+		private RollaBall.PlayerMove Pause;
+		
+		IEnumerator SetCollider(PlayerMove obj, bool value, float time) {
+			yield return new WaitForSeconds(time);
+			obj.enabled = value;
+		}
+		
+		private void OnMouseDown() {
+			Pause = GetComponent<PlayerMove>();
+			StartCoroutine(SetCollider(GetComponent<PlayerMove>(), false, 2.5f));
+			StartCoroutine(SetCollider(GetComponent<PlayerMove>(), true, 2.5f));
+			Pause.enabled = !Pause.enabled;
+			
+			animObject.Rewind("playerAnim");
+			animObject.Play("playerAnim");
+		}
+	}
+}
