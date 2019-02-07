3d3368c6ddc7b420599112c453cf852149a1807f
Segei-Vasiliev <v-ser-v@yandex.ru>
2019-02-07 12:41:59 +0500

    Задача Создания инфо-панели по клику на облака

---
+++ Assets/_COMIRON/Scripts/Ui/Panels/PanelCloudInfo/PanelCloudInfo.cs
@@ -0,0 +1,27 @@
+﻿using COMIRON.GameFramework.Ui;
+using UnityEngine;
+
+namespace COMIRON.Ui.Panels {
+	public class PanelCloudInfo : PanelBase {
+		public event System.Action OnActionButtonCloseClick;
+
+		[SerializeField]
+		private ButtonBase buttonClose;
+
+		protected override void InitializeInherit() {
+			this.buttonClose.OnActionClick += delegate {
+				if (this.OnActionButtonCloseClick != null) {
+					this.OnActionButtonCloseClick();
+				}
+			};
+		}
+
+		protected override void EnableInherit() {
+
+		}
+
+		protected override void DisableInherit() {
+
+		}
+	}
+}
