5ee9a5cf560a802db2f6beae00ba0a8d8283ffaf
Victor <vnktor@mail.ru>
2019-02-20 11:40:48 +0600

    SocBase, удалил не используемую локальную переменную.

--- Assets/_COMIRON/Scripts/_GameFramework/Soc/SocBase.cs
+++ Assets/_COMIRON/Scripts/_GameFramework/Soc/SocBase.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 namespace COMIRON.GameFramework.Soc {
 	public abstract class SocBase : MonoBehaviour {
-		private GameEngineBase gameEngine;
-
 		private List<SocBase> socList;
 
 		public void Initialize(GameEngineBase gameEngine) {
-			this.gameEngine = gameEngine;
-		
 			this.InitializeInherit();
 		}
 
