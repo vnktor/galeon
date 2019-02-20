ac6ce4375fb393629dd8d5815624d3097930518c
ssnshkl <11noabrya@mail.ru>
2019-02-20 12:02:48 +0500

    табуляция

--- Assets/_COMIRON/Scripts/Scenes/GameEngineMain.cs
+++ Assets/_COMIRON/Scripts/Scenes/GameEngineMain.cs
@@ -51,7 +51,8 @@ namespace COMIRON.Scenes {
 
 			this.CreateClouds(startCloudPosition, endCloudPosition);
 
-			for (int i = 0; i < 10; i++) {
+			for (int i = 0; i < 10; i++)
+			{
 				this.CreateTree();
 			}
 
